349ca830ed9a490d49d043c5d49e07a848c7f01d
unknown <mainali_ayush@outlook.com>
2019-02-04 12:47:30 -0600

    'a'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './products/products.component';
 import { observable } from 'rxjs';
+import { ProductsComponent } from './products/products.component';
+import { ProductSearchPipe } from './products/productsearchpipe.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductSearchPipe,
     ProductsComponent
   ],
   imports: [
