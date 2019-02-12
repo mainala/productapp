c05b86c784fe9d5f67bb1b309c9110f54d7c88ee
Ayush Mainali <amainali@aims1.com>
2019-02-12 17:26:50 -0600

    'angular-routing'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { observable } from 'rxjs';
+import {RouterModule} from '@angular/router';
+
 import { ProductsComponent } from './products/products.component';
 import { ProductSearchPipe } from './products/productsearchpipe.pipe';
 import { DiscountPipe } from './products/discountPipe.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductService } from './products/product.service';
+import {  OrderComponent } from './orders/order.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { NotFoundComponent } from './shared/NotFound.Component';
 
 
 @NgModule({
@@ -16,11 +22,23 @@ import { ProductService } from './products/product.service';
     ProductSearchPipe,
     DiscountPipe,
     ProductsComponent,
-    StarComponent
+    StarComponent,
+    HomeComponent,
+    OrderComponent,
+    ProductDetailComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'products', component:ProductsComponent},
+      {path:'products/:id', component:ProductDetailComponent},
+      {path:'orders', component:OrderComponent},
+      {path: 'home', component:HomeComponent},
+      {path: '', redirectTo:'home', component:ProductsComponent, pathMatch:'full'},
+      {path: '**', component:NotFoundComponent}
+    ])
   ],
   providers: [
     ProductService
